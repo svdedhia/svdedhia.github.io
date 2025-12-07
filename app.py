15db2bf4046700e95130a7448907aa1addac7a33
Kylie (Kanade) Ueda <uedak@etown.edu>
2025-12-06 20:21:50 -0500

    had basic flask app built

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-# build a Flask app
+'''
+# Note
+app -> web app itself
+__name__ -> parameter
+'''
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/WheatDisease')
+
+def WheatDisease():
+    return {
+        'status' : 'ok'
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
