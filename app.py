233e29af26de0077d64a41e22ef285c2375025ac
Kylie (Kanade) Ueda <uedak@etown.edu>
2025-12-09 16:40:17 -0500

    Installed libraries, make it accesible from browsers, andadded function to load model

--- app.py
+++ app.py
@@ -4,14 +4,28 @@ app -> web app itself
 __name__ -> parameter
 '''
 from flask import Flask
+from flask_cors import CORS
+import tensorflow as tf
+
 app = Flask(__name__)
+CORS(app) # -> make it accessible from browsers
+
+MODEL_PATH = 'wheat_mobilenet_finetuned.h5'
+model = None
+
+def load_model():
+    global model
+    try:
+        model = tf.keras.models.load_model(MODEL_PATH)
+        print("Successfully loaded a model!")
+    except Exception as e:
+        print(f"Error!: {e}")
 
 @app.route('/WheatDisease')
 
 def WheatDisease():
-    return {
-        'status' : 'ok'
-    }
+    return { 'status' : 'ok', 'model_loaded': model is not None}
 
 if __name__ == '__main__':
+    load_model()
     app.run(debug=True)
